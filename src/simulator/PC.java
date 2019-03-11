fdc73e69dabf0c711b7b94d4aa386f5d314d4c73
Amrit-pal-Singh <2017csb1068@iitrpr.ac.in>
2019-03-11 20:10:35 +0530

    added the PC class

---
+++ src/simulator/PC.java
@@ -0,0 +1,33 @@
+/*
+    to use this class
+    first set_pc, set_muxPc, set_muxInc
+    then use adder which will return the next pc
+*/
+package com.company;
+// import class containg RA and RB;
+
+public class PC {
+    int pc = 0x0, pc_temp;  // default value of PC set to 0
+    int muxPc, muxInc = 4;  // default value of MuxINC is 4
+
+    // default value is 0x0
+    void set_pc(int pc_value){
+        pc = pc_value;
+    }
+
+    // RA or
+    void set_muxPc(int Mux_PC_value){
+        muxPc = Mux_PC_value;
+    }
+
+    // default value is 4
+    void set_muxInc(int Mux_INC_value){
+        muxInc = Mux_INC_value;
+    }
+
+    int adder(){
+        pc_temp = pc;
+        pc = pc+muxInc;
+        return pc;
+    }
+}
